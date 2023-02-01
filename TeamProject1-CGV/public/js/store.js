684fbeae3b4db12de469981cb17d6636760c842d
YUSEULBI <ha258ppy@gmail.com>
2023-02-02 00:04:39 +0900

    23-02-01 밤11:59 store html , css , js

--- TeamProject1-CGV/public/js/store.js
+++ TeamProject1-CGV/public/js/store.js
@@ -1,3 +1,12 @@
-/**
- * 
- */
+console.log('js실행')
+
+let categoryList = [  '팝콘' , '음료' , '콤보' , '스낵' , '영화관람권' , '기프트카드' , '플레이존' , '상품권소개' ]
+
+let productList = [
+	
+	{ category: '콤보' , name : '더블콤보'  	},
+	{ category: '콤보' , name : '라지콤보' 	},
+	{ category: '콤보' , name : '스몰세트' 	},
+	{ category: '콤보' , name : 'CGVOX콤보' 	}
+	
+]
