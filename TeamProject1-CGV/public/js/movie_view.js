a2d308d463c7963ca58e28e9c74b099834d432e0
504 <504@DESKTOP-0J3Q3S4>
2023-02-03 16:26:08 +0900

    23-02-03 16:25 정소연

--- TeamProject1-CGV/public/js/movie_view.js
+++ TeamProject1-CGV/public/js/movie_view.js
@@ -1,7 +1,8 @@
+
 let movieList=[
 	{name:'BTS: Yet To Come in Cinemas',img:'방탄콘서트.jpg',감독 :'오윤동' 	   , 장르 : '공연실황', 	평점:8.83, 개봉일:'20230201', 상영시간: 120},
 	{name:'아바타-물의 길',		img:'아바타.jpg',			감독 :'James Cameron'   , 장르 : 'SF', 		평점:8.83, 개봉일:'20221214', 상영시간: 120},
-	{name:'오늘 밤,세계에서 이 사랑이 사라진다해도',	img:'오늘밤사라진다해도.jpg',	감독 :'Takahiro Miki' , 장르 : 'SF', 	평점:8.83, 개봉일:'20221214', 상영시간: 120},
+	{name:'오늘 밤,세계에서 이 사랑이 사라진다해도',	img:'오늘밤.jpg',	감독 :'Takahiro Miki' , 장르 : 'SF', 	평점:8.83, 개봉일:'20221214', 상영시간: 120},
 	{name:'영웅',				img:'영웅.jpg',			감독 :'윤제균' 		   , 장르 : '드라마', 		평점:8.42, 개봉일:'20221221', 상영시간: 120},
 	{name:'교섭',				img:'교섭.jpg',			감독 :'임순례' 		   , 장르 : '드라마', 		평점:6.29, 개봉일:'20230118', 상영시간: 120},
 	{name:'상견니',			img:'상견니.jpg',			감독 :'Tianren Huang'   , 장르 : '드라마', 		평점:7.85, 개봉일:'20230125', 상영시간: 120},
@@ -28,7 +29,7 @@ let setlist2 = [
 {movie:'아바타', space : '안산', room: '5관',  time: '16:00'},
 ]
 
-
+let times=[{time: '10:00'},{time: '11:30'}]
 //--영화 리스트 출력-----------------------------------------------------------------------------------------------------//
 
 list()
@@ -93,12 +94,12 @@ function print(i){
 					<th width="20%" > 지역 </th> <th width="20%" > 상영관 </th> <th width="60%" > 시간 </th>
 				</tr>`;
 
-	 	setlist2.forEach((o)=>{
+	 	setlist2.forEach((o,i)=>{
 
 			html += `<tr>
 						<th> ${o.space}  </th>
 						<th> ${o.room } </th>
-						<th> <button class="time" onclick="select()"> ${o.time}</button>   </th>
+						<th> <button class="time" onclick="select(${i})"> ${o.time}</button>   </th>
 					</tr>`
 		
 			})	// for e
@@ -110,23 +111,48 @@ function print(i){
 	 }	// fun e
 //------------------------------------------------------------------------------------------------------------------//			
 
- 
-//------------------------------------------------------------------------------------------------------------------//
+function select (i){
+	
+	
+	let 날짜     = document.querySelector('.date').value
+	let 지역  	= setlist2[i].space
+	let 관		= setlist2[i].room
+	let 시간 		= setlist2[i].time
 
+	if(날짜==''){alert('날짜를 선택해 주세요.'); return;}
+	
+	
+	document.querySelector('.movie').innerHTML	= document.querySelector('.m_title').innerHTML
+	document.querySelector('.space').innerHTML	= `CGVOX  `+지역
+	document.querySelector('.room').innerHTML	= 관
+	document.querySelector('.day').innerHTML	= 날짜
+	document.querySelector('.tim').innerHTML	= 시간
+	
+	
+}
+//----에매 내역 모달 내용 출력----------------------------------------------------------------------------------------------//
 
+// 예매하기 버튼 클릭 이벤트
  document.querySelector('.reserve').addEventListener('click',(e)=>{
 	 
 	 document.querySelector('.modal_wrap').style.display='flex';
+	 modal_print()
  })
- 
+
+// 예매내역 
+function modal_print(){
+	
+	document.querySelector('.m_space').innerHTML	= document.querySelector('.space').innerHTML
+	document.querySelector('.m_movie').innerHTML	= document.querySelector('.m_title').innerHTML
+	document.querySelector('.m_room').innerHTML		= document.querySelector('.room').innerHTML
+	document.querySelector('.m_day').innerHTML		= document.querySelector('.day').innerHTML
+	document.querySelector('.m_time').innerHTML		= document.querySelector('.tim').innerHTML
+	document.querySelector('.m_price').innerHTML	= `16,000원`
+	
+}
+
+// 모달 버튼 이벤트
  document.querySelector('.modal_cancel').addEventListener('click',(e)=>{
 	 
 	 document.querySelector('.modal_wrap').style.display='none';
  })
- 
- document.querySelector('.modal_close').addEventListener('click',(e)=>{
-	 
-	 document.querySelector('.modal_wrap').style.display='none';
- })
-
-
